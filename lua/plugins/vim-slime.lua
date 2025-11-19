dc4a2e14f7154e543bb38a60a814a5237f08a2d0
I540823 <nikita.zhukov@sap.com>
2025-11-19 21:39:45 +0100

    Add Copilot & VimSlime

---
+++ lua/plugins/vim-slime.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"jpalardy/vim-slime",
+		keys = {
+			{ "<leader>rC", "<cmd>SlimeConfig<cr>", desc = "Slime Config" },
+			{ "<leader>rr", "<Plug>SlimeSendCell<BAR>/^# %%<CR>", desc = "Slime Send Cell" },
+			{ "<leader>rr", ":<C-u>'<,'>SlimeSend<CR>", mode = "v", desc = "Slime Send Selection" },
+		},
+		config = function()
+			vim.g.slime_target = "tmux"
+			vim.g.slime_cell_delimiter = "# %%"
+			vim.g.slime_bracketed_paste = 1
+		end,
+	},
+}
